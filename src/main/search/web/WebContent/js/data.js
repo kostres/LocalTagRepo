490271153d9f9ef57c9c36323efde611cb6375ef
Nebojša Taušan <kostres@gmail.com>
2014-10-20 15:39:20 +0300

    summ
    desc

--- src/main/search/web/WebContent/js/data.js
+++ src/main/search/web/WebContent/js/data.js
@@ -2,17 +2,17 @@ var data = {
 	links : [ 
 		{
 		text : 'Essentially, it is a method of teaching computers to make and improve predictions or behaviors based on some data. What is this data? Well, that depends entirely on the problem. It could be readings from a robots sensors as it learns to walk, or the correct output of a program for certain input.',
-		label : 'machine learning, data mining',
+		label : 'machine learning ',
 		url : "http://stackoverflow.com/questions/2620343/what-is-machine-learning"
 	}, {
 		text:"This is probably the closest to Code Complete of AI. Peter Norvig",
-		label : 'machine learning, AI',
+		label : 'AI',
 		url : "http://stackoverflow.com/questions/821204/best-books-blogs-link-reading-about-ai-and-machine-learning"
 
 	}, 
 	{
 		text : "A folksonomy is a type of distributed classification system. It is usually created by a group of individuals, typically the resource users. Users add tags to online items, such as images, videos, bookmarks and text. ",
-		label : 'tagging, folksonomy',
+		label : 'folksonomy',
 		url : "http://www.dlib.org/dlib/january06/guy/01guy.html"
 
 	}
@@ -23,6 +23,25 @@ var data = {
 		url : "http://2.bp.blogspot.com/_9GHoR-RJLy8/S8WQ-Tnd-SI/AAAAAAAAQnk/ztXkONoybQk/s1600/Finlandpic.jpg"
 
 	}
-	
+	, 
+	{
+		text : "Selected text goes here... ",
+		label : 'data mining',
+		url : "http://2.bp.blogspot.com/_9GHoR-RJLy8/S8WQ-Tnd-SI/AAAAAAAAQnk/ztXkONoybQk/s1600/Finlandpic.jpg"
+
+	}	
+	, 
+	{
+		text : "Selected text goes here... keep this in mind",
+		label : 'vuk bubnja',
+		url : "http://2.bp.blogspot.com/_9GHoR-RJLy8/S8WQ-Tnd-SI/AAAAAAAAQnk/ztXkONoybQk/s1600/Finlandpic.jpg"
+
+	}, 
+	{
+		text : "Asigning a distinct name to the piece of text...  ",
+		label : 'tagging',
+		url : "http://www.dlib.org/dlib/january06/guy/01guy.html"
+
+	}	
 	]
 };
